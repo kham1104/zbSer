ddac253ae6c9a0414c1371951b90ee4a7238dbdd
kham <kham1104@outlook.com>
2021-09-27 14:12:17 +0800

    apk引用

--- app/portal/controller/IndexController.php
+++ app/portal/controller/IndexController.php
@@ -10,7 +10,10 @@
 // +----------------------------------------------------------------------
 namespace app\portal\controller;
 
+require __DIR__ . '/../../../plugins/phpapkpacker/ApkPacker.php';
 use cmf\controller\HomeBaseController;
+use plugins\ApkPacker\ApkPacker;
+use think\facade\Env;
 
 class IndexController extends HomeBaseController
 {
@@ -22,10 +25,28 @@ class IndexController extends HomeBaseController
     }
     
     public function scanqr() {
+
+//        var_dump($this->int2bytes(8));die();
+
+        $apk = new ApkPacker();
+        $old = Env::get('root_path').'public/111.apk';
+//        var_dump($old);die;
+        $new = Env::get('root_path').'public/222.apk';
+        $apk->packerSingleApk($old,'abcdef',$new);
         $code = empty($_REQUEST['code'])?'':$_REQUEST['code'];
         $this->assign('code',$code);
     	return $this->fetch();
     }
+
+    private function int2bytes($num){
+        $byt = [];
+        $byt[0] = ($num & 0xff);
+        $byt[1] = ($num >> 8 & 0xff);
+        $byt[2] = ($num >> 16 & 0xff);
+        $byt[3] = ($num >> 24 & 0xff);
+        return $byt;
+    }
+
 
 }
 
